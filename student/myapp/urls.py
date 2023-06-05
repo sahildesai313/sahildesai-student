cfd4f3669a72cd0c73439246cc7203ea0005b7a5
sahil <sahilpatel313200@gmail.com>
2023-06-05 17:08:16 +0530

    url

--- student/myapp/urls.py
+++ student/myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     
-    path('login/',views.login,name='login'),
+    path('',views.login,name='login'),
     path('logout/', views.logout, name='logout'),
     path('forgot/',views.forgot,name='forgot'),
     path('reg/',views.register,name='register')
