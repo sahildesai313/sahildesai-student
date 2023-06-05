2cc1a67749b5e6a595f30cbcfcf7648cf274d6d7
sahil <sahilpatel313200@gmail.com>
2023-06-05 16:29:56 +0530

    second commit

--- student/myapp/admin.py
+++ student/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Register
 
-# Register your models here.
+    
+class RegisterAdmin(admin.ModelAdmin):
+  list_display = ("username","firstname", "lastname","phone","email","password","confirmpassword")
+  
+
+  
+
+
+admin.site.register(Register,RegisterAdmin)
