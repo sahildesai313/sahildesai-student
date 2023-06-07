0c8cbf36cd43c6412e9a4c785979411910254911
kishan <kishandobariya741@gmail.com>
2023-06-07 12:23:13 +0530

    homepage&models-data

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('',views.login,name='login'),
@@ -9,4 +11,6 @@ urlpatterns = [
     path('profile/',views.profile,name="profile"),
     path('reg/',views.register,name='register'),
     path('home/',views.homepage,name='home'),
-]
+    path('home/tour_detail/',views.tour,name='tour'),
+   
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
