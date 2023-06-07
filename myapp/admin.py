c2ef6d47783eba26a442865b343f0fe5c093d5eb
kishandobariy <112072794+kishandobariy@users.noreply.github.com>
2023-06-07 16:38:04 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import UserDetail ,package_details
-from .models import Image
-
-class imageAdmin(admin.ModelAdmin):
-    list_display = ["title", "photo"]
-
-admin.site.register(Image, imageAdmin)
 
 
 class UserDetailAdmin(admin.ModelAdmin):
